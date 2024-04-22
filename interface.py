a14b5f3632ae2ab2fc31fb360bbd3354915fc95d
Ibensouda <127936182+Ibensouda@users.noreply.github.com>
2024-04-22 15:43:42 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -8,11 +8,8 @@ def place_point(event):
     canvas.create_oval(x-3, y-3, x+3, y+3, fill="blue", outline="", tags="point")
     label.config(text=f"Clicked at ({x}, {y})")
 
-# Création de la fenêtre principale
 root = tk.Tk()
 root.title("Interface avec image")
-
-# Obtenir la résolution de l'écran
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 
@@ -28,7 +25,7 @@ image_height = image.height
 center_x = screen_width // 2
 center_y = screen_height // 2
 
-# Création d'un canvas pour dessiner
+# Canvas pour dessiner
 canvas = tk.Canvas(root, width=screen_width-20, height=screen_height-20, bg="white")
 canvas.pack()
 canvas.create_image(center_x - image_width // 2, center_y - image_height // 2, anchor=tk.NW, image=photo)
@@ -40,11 +37,9 @@ canvas.create_oval(center_x-3, center_y-3, center_x+3, center_y+3, fill="blue", 
 label = tk.Label(root, text=f"Clicked at ({center_x}, {center_y})")
 label.pack()
 
-# Lier l'événement de clic sur l'image à la fonction place_point
 canvas.bind("<Button-1>", place_point)
 
-# Activer le mode plein écran
+# plein écran
 root.attributes("-fullscreen", True)
 
-# Lancement de la boucle principale
 root.mainloop()
